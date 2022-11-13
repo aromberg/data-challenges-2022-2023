c425f8b0fb2a7ee1face19217e5490de71fd2f33
Adrian Romberg <adrian.romberg@icloud.com>
2022-11-13 19:05:48 +0100

    fix typo

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.markdown(
     This data challenge is about finding differences in how different genders
     are affected by chronic diseases.
     ### Why is this important?
-    - 💢 Studies have found **significant differences** in how men and women are affectedby chronic diseases: prevalence, degree of severity, symptoms- biological differences & gender aspects
+    - 💢 Studies have found **significant differences** in how men and women are affected by chronic diseases: prevalence, degree of severity, symptoms- biological differences & gender aspects
     - 👫 Gender differences not only have a **direct impact on the health behaviour, exposures, social factors**, but also on needs and access to care of women and men
     - 🏥 **Governments and health systems** do **not always recognize** or **address** this sufficiently: health systems can also indirectly contributes to gender inequalities in health
 
