82e14f7530157d0a24be8ca5cb1415cd6148a3f7
Adrian Romberg <adrian.romberg@icloud.com>
2022-11-14 16:49:14 +0100

    refactor data imputation

--- pages/05_Data imputation.py
+++ pages/05_Data imputation.py
@@ -2,164 +2,123 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.express as px
 
 st.set_page_config(page_title="Data Imputation",
                    page_icon="🎛️")
 
 st.markdown("# Data Imputation")
 
-st.markdown('We printed a preview of the dataset.')
+st.markdown('Printing a preview of the datasets reveals missing data:')
+
 # load dataset
 data = pd.read_csv("data/heart_disease/heart_disease_combined.csv", sep=",",
  index_col=0)
 st.dataframe(data.head())
 
+# lists for discrete/continuous data
+cont_columns = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+disc_columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal', 'num']
+
+st.markdown(
+    """
+    ## Comparing male and female NaNs
+    Because this project is about finding differnces between genders, we \
+    decided to see if differences already exists and this stage, and if \
+    imputation should thus be done separately:
+
+    ### Histograms
+    """
+)
+# plot attributes to get a first impression of them before imputing
+compare_option = st.selectbox(
+    'Which column would you like to plot?',
+    data.columns)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("#### Male")
+    hist = px.histogram(data[data['sex'] == 1], x=compare_option)
+    st.plotly_chart(hist, use_container_width=True)
+
+with col2:
+    st.write("#### Female")
+    hist = px.histogram(data[data['sex'] == 0], x=compare_option)
+    st.plotly_chart(hist, use_container_width=True)
+
+
+st.write("### Means")
 st.markdown('We calculated the means for each continuous variable for both \
  male and female to identify differences in the data. We can see that data \
- of variable "trestbps" seems to be similiar for males and females. That \
- is why we deleted this column.')
-
-data_male = data[data['sex'] == 1]
-data_female = data[data['sex'] == 0]
-
-means_male = data_male.mean()
-means_female = data_female.mean()
+ of variable "trestbps" seems to be similiar for males and females.')
+means_male = data[data['sex'] == 1].mean()
+means_female = data[data['sex'] == 0].mean()
 
 means = pd.DataFrame([means_male, means_female])
 means.index = ['male', 'female']
-means = means.drop(['age','sex','cp','fbs', 'restecg','exang','slope','thal',
-'num','ca'], axis=1)
-st.dataframe(means)
+means = means.drop(disc_columns, axis=1)
+st.write(means)
 
-# delete variables where is no difference in male and female
-data_imp = data.copy()
-data_imp = data_imp.drop(columns=['trestbps'])
 
-st.markdown('## Mean Imputation for "chol"')
-st.markdown('The blue datapoints represent the complete datapoints and \
-and the orange datapoints represent the datapoints where we imputet the mean.')
-data_imp['chol'] = data_imp['chol'].fillna(data_imp['chol'].mean())
-# visualize results
-fig = plt.figure()
-plt.scatter(data_imp['age'], data_imp['chol'])
-plt.scatter(data_imp[np.isnan(data['chol'])]['age'],
- data_imp[np.isnan(data['chol'])]['chol'])
-plt.xlabel('age')
-plt.ylabel('chol')
-st.pyplot(fig)
-
-st.markdown('## Mean Imputation for "thalach"')
-data_imp['thalach'] = data_imp['thalach'].fillna(data_imp['thalach'].mean())
-# visualize results
-fig = plt.figure()
-plt.scatter(data_imp['age'], data_imp['thalach'])
-plt.scatter(data_imp[np.isnan(data['thalach'])]['age'],
- data_imp[np.isnan(data['thalach'])]['thalach'])
-plt.xlabel('age')
-plt.ylabel('thalach')
-st.pyplot(fig)
-
-st.markdown('## Mean Imputation for "oldpeak"')
-data_imp['oldpeak'] = data_imp['oldpeak'].fillna(data_imp['oldpeak'].mean())
-# visualize results
-fig = plt.figure()
-plt.scatter(data_imp['age'], data_imp['oldpeak'])
-plt.scatter(data_imp[np.isnan(data['oldpeak'])]['age'],
- data_imp[np.isnan(data['oldpeak'])]['oldpeak'])
-plt.xlabel('age')
-plt.ylabel('oldpeak')
-st.pyplot(fig)
-
+st.write("### Modes")
 st.markdown('Analogous to the continuous variable we calculated the modes for \
 each categorial variable for both male and female and delete all columns \
 which indicate no difference.')
-modes_male = data_male.mode().iloc[0]
-modes_female = data_female.mode().iloc[0]
+modes_male = data[data['sex'] == 1].mode().iloc[0]
+modes_female = data[data['sex'] == 0].mode().iloc[0]
 
 modes = pd.DataFrame([modes_male, modes_female])
 modes.index = ['male', 'female']
-modes = modes.drop(['age','sex','trestbps','chol','thalach','oldpeak','num'],
- axis=1)
-st.dataframe(modes)
+modes = modes.drop(cont_columns, axis=1)
+st.write(modes)
 
-# delete variables where is no difference in male and female
-data_imp = data_imp.drop(columns=['cp','fbs','restecg','exang','slope','ca'])
+# Imputation
+data_imp_male = data.copy()[data['sex'] == 1]
+data_imp_female = data.copy()[data['sex'] == 0]
 
-st.markdown('## Mode Imputation for "thal"')
+
+st.write("## Mean & mode imputation")
+st.markdown('Because significant differences are visible when comparing male\
+             and female data, we decided to impute missing values separately.')
+
+# mean imputation
+male_imp_dict = means.to_dict(orient='records')[0]
+female_imp_dict = means.to_dict(orient='records')[1]
+data_imp_male.fillna(male_imp_dict, inplace=True)
+data_imp_female.fillna(female_imp_dict, inplace=True)
+# st.write(data_imp_female)
+
 # mode imputation
-data_imp['thal'] = data_imp['thal'].fillna(int(data_imp['thal'].mode()))
-null_values = data['thal'].isnull()
+male_imp_dict = modes.to_dict(orient='records')[0]
+female_imp_dict = modes.to_dict(orient='records')[1]
+data_imp_male.fillna(male_imp_dict, inplace=True)
+data_imp_female.fillna(female_imp_dict, inplace=True)
 
-# visualize results
-fig = plt.figure()
-plt.scatter(data_imp['age'], data_imp['thal'])
-plt.scatter(data_imp[np.isnan(data['thal'])]['age'],
- data_imp[np.isnan(data['thal'])]['thal'])
-plt.xlabel('age')
-plt.ylabel('thal')
-st.pyplot(fig)
+data_imp = pd.concat([data_imp_male, data_imp_female]).sort_index()
 
-st.markdown('## Mean Imputation for subgroups')
-st.markdown('To check how the imputation values differ if we apply them to \
-subgroups we divided the dataset in males and females.')
-data_imp_male = data[data['sex']==1].copy()
-data_imp_female = data[data['sex']==0].copy()
+st.write('## Results')
 
-data_imp_male['chol'] = \
-data_imp_male['chol'].fillna(data_imp_male['chol'].mean())
-data_imp_female['chol'] = \
-data_imp_female['chol'].fillna(data_imp_female['chol'].mean())
-
-data_imp_male['thalach'] = \
-data_imp_male['thalach'].fillna(data_imp_male['thalach'].mean())
-data_imp_female['thalach'] = \
-data_imp_female['thalach'].fillna(data_imp_female['thalach'].mean())
-
-data_imp_male['oldpeak'] = \
-data_imp_male['oldpeak'].fillna(data_imp_male['oldpeak'].mean())
-data_imp_female['oldpeak'] = \
-data_imp_female['oldpeak'].fillna(data_imp_female['oldpeak'].mean())
-
-data_imp_male['thal'] = \
-data_imp_male['thal'].fillna(data_imp_male['thal'].mode())
-data_imp_female['thal'] = \
-data_imp_female['thal'].fillna(data_imp_female['thal'].mode())
-
-# visualize results
-fig = plt.figure()
-plt.scatter(data_imp_male['age'], data_imp_male['oldpeak'])
-plt.scatter(data_imp_male[np.isnan(data[data['sex']==1]. \
-copy()['oldpeak'])]['age'],data_imp_male[ \
-np.isnan(data[data['sex']==1].copy()['oldpeak'])]['oldpeak'])
-plt.xlabel('age')
-plt.ylabel('oldpeak')
-plt.title('Mean Imputation for Males')
-st.pyplot(fig)
+# dropdown for chart
+imputed_option = st.selectbox(
+    'Which column would you like to plot to see imputations?',
+    data_imp.columns,
+    index=4)
 
 fig = plt.figure()
-plt.scatter(data_imp_female['age'], data_imp_female['oldpeak'])
-plt.scatter(data_imp_female[np.isnan(data[data['sex']==0]['oldpeak'])]['age'],
-    data_imp_female[np.isnan(data[data['sex']==0]['oldpeak'])]['oldpeak'])
+plt.scatter(data_imp['age'], data_imp[imputed_option])
+plt.scatter(data_imp[np.isnan(data[imputed_option])]['age'],
+ data_imp[np.isnan(data[imputed_option])][imputed_option])
 plt.xlabel('age')
-plt.ylabel('oldpeak')
-plt.title('Mean Imputation for Females')
+plt.ylabel(imputed_option)
 st.pyplot(fig)
+
+with st.expander("See imputed dataset"):
+    st.write(data_imp)
+
 
 # save results
 from pathlib import Path
 filepath = Path('data/data_imp.csv')
 filepath.parent.mkdir(parents=True, exist_ok=True)
 data_imp.to_csv(filepath, index=False)
-
-# concat male and female datasets
-data_imp = pd.concat([data_imp_male, data_imp_female]).sort_index()
-fig = plt.figure()
-plt.scatter(data_imp['age'], data_imp['oldpeak'])
-plt.scatter(data_imp[np.isnan(data['oldpeak'])]['age'],
-    data_imp[np.isnan(data['oldpeak'])]['oldpeak'])
-plt.xlabel('age')
-plt.ylabel('oldpeak')
-plt.title('Mean Imputation for Males and Females')
-st.pyplot(fig)
-
-
