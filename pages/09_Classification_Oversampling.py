1d0987fa60ccbc42d87910616bde52592ba011f3
Katharina Lenhart <k.lenhart@outlook.com>
2023-01-21 18:28:52 +0100

    t

--- pages/09_Classification_Oversampling.py
+++ pages/09_Classification_Oversampling.py
@@ -36,5 +36,5 @@ X_test_without_sex = X_test.drop(['sex'], axis=1)
 training_dataset = pd.concat([X_train, y_train], axis=1)
 class_distr = training_dataset.groupby('sex')['hd'].value_counts().unstack()
 class_distr = class_distr.rename(index={0: 'Female', 1: 'Male'}, columns={0: 'No', 1: 'Yes'})
-
-st.bar_chart(class_distr.transpose(), xlab="hd")
+class_distr
+st.write(class_distr[0])
