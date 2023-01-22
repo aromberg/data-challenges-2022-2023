20540401d9e0a0392bb67bad030fb72711131598
Katharina Lenhart <k.lenhart@outlook.com>
2023-01-22 16:41:34 +0100

    first part done

--- pages/09_Classification_Oversampling.py
+++ pages/09_Classification_Oversampling.py
@@ -6,6 +6,7 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 import numpy as np
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import SMOTE, ADASYN
 from collections import Counter
 
 def calculate_metrics(y_test, y_pred):
@@ -19,7 +20,7 @@ def calculate_metrics(y_test, y_pred):
 
     return f1_str, auc_str
 
-def create_decision_tree(data, X_train, y_train, X_test, y_test):
+def create_decision_tree(X_train, y_train):
     # create inital decision tree
     decision_tree = tree.DecisionTreeClassifier(random_state=0)
     decision_tree = decision_tree.fit(X_train, y_train)
@@ -49,9 +50,8 @@ Unbalanced datasets are those in which an uneven distribution of classes, a so-c
 or bias, is found. One method to improve the distribution of training examples is to add or remove
 examples in a process called resampling. Here, we will apply ovesampling methods which expand the partial
 dataset of the minority class.
-## Check class distribution in training dataset:
-To check for skewness we'll plot the distribution of our training dataset based on class labels and gender
-and calculate the sample skewness:
+## Check class distribution in training dataset
+To check for skewness we'll plot the distribution of our training dataset based on class labels and gender:
 """)
 
 data = pd.read_csv("data/data_hr.csv")
@@ -103,10 +103,91 @@ st.markdown(
 """
 ## Apply Oversampling Methods
 As the partial dataset with class labels "no hd" reflects the minority, we will add samples by oversampling.
-### Random oversampling
+### Decision Tree without Gender Feature
 """
 )
 ros = RandomOverSampler(random_state=2023)
 X_tros, y_tros = ros.fit_resample(X_train, y_train)
-st.write('Original dataset shape %s' % Counter(y_train))
-st.write('Resampled dataset shape %s' % Counter(y_tros))
+X_smote, y_smote = SMOTE(random_state=2023).fit_resample(X_train, y_train)
+# transform datasets to df
+data_tros = pd.concat([X_tros, y_tros], axis=1)
+data_test = pd.concat([X_test, y_test], axis=1)
+data_ros = pd.concat([data_tros, data_test], axis=0)
+
+data_tsmote = pd.concat([X_smote, y_smote], axis=1)
+data_smote = pd.concat([data_tsmote, data_test], axis=0)
+
+X_tros_without_sex = X_tros.drop(['sex'], axis=1)
+X_test_without_sex = X_test.drop(['sex'], axis=1)
+
+X_smote_without_sex = X_smote.drop(['sex'], axis=1)
+# create basic decision tree without sex feature
+decision_tree_without_sex_ros = create_decision_tree(X_train=X_tros_without_sex, y_train=y_tros)
+decision_tree_without_sex_smote = create_decision_tree(X_train=X_smote_without_sex, y_train=y_tros)
+# predict targets
+y_ros_pred_without_sex = decision_tree_without_sex_ros.predict(X_test_without_sex)
+y_smote_pred_without_sex = decision_tree_without_sex_smote.predict(X_test_without_sex)
+
+meth_wosex = st.selectbox(
+    label="Choose an oversampling method prior to generating decision-trees w/o sex information",
+    options=["Random Oversampler", "SMOTE"],
+    help="https://imbalanced-learn.org/stable/over_sampling.html"
+)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+if meth_wosex == "Random Oversampler":
+    st.write("A naive strategy to generate new samples is by randomly sampling with replacement.\
+        Examples from the minority class are randomly selected and added to the training dataset.")
+    tp = tree.plot_tree(decision_tree_without_sex_ros,
+                        filled=True,
+                        rounded=True,
+                        class_names=['no_hd','yes_hd'],
+                        feature_names=X_encode.columns)
+    st.subheader("Confusion Matrix for Decision Tree without Gender Feature after random oversampling")
+    cm_without_sex_ros = metrics.confusion_matrix(y_true=y_test, y_pred=y_ros_pred_without_sex)
+    disp_ros = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_without_sex_ros)
+    disp_ros.plot()
+else:
+    st.write("The **Synthetic Minority Oversampling Technique** (SMOTE) works by selecting examples\
+             that are close in the feature space (neighbors), drawing a line between the examples in the feature\
+            space and drawing a new sample at a point along that line.")
+    tp = tree.plot_tree(decision_tree_without_sex_smote,
+                        filled=True,
+                        rounded=True,
+                        class_names=['no_hd','yes_hd'],
+                        feature_names=X_encode.columns)
+    st.subheader("Confusion Matrix for Decision Tree without Gender Feature after SMOTE")
+    cm_without_sex_smote = metrics.confusion_matrix(y_true=y_test, y_pred=y_smote_pred_without_sex)
+    disp_smote = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_without_sex_smote)
+    disp_smote.plot()
+
+st.pyplot()
+
+st.markdown(
+"""Again, we evaluated the performance of the decision tree with a confusion matrix and by calculating
+the area under the ROC curve (AUC) and the F-Score.
+"""
+)
+f1 = 83.3
+auc = 0.78
+f1_ros, auc_ros = calculate_metrics(y_test, y_ros_pred_without_sex)
+f1_smote, auc_smote = calculate_metrics(y_test, y_smote_pred_without_sex)
+
+col1, col2, col3 = st.columns(3)
+col1.write("🙅🏻‍♀️ No Oversampling")
+with col1:
+     st.metric(label="F-Score", value=f1)
+     st.metric(label="AUC", value=auc)
+col2.write("🎲 Random Oversampling")
+with col2:
+     st.metric(label="F-Score", value=f1_ros)
+     st.metric(label="AUC", value=auc_ros)
+col3.write("🏘 SMOTE")
+with col3:
+     st.metric(label="F-Score", value=f1_smote)
+     st.metric(label="AUC", value=auc_smote)
+
+st.markdown(
+"""Again, we evaluated the performance of the decision tree with a confusion matrix and by calculating
+the area under the ROC curve (AUC) and the F-Score.
+"""
+)
